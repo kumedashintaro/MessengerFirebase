8f233d392b7105d2a365e63f9194b66f5c1236d7
kumedashintarou <pii.k2.bys@gmail.com>
2020-04-17 22:16:35 +0900

    textに丸みを追加

--- app/src/main/java/com/example/messengerfirebase/LoginActivity.kt
+++ app/src/main/java/com/example/messengerfirebase/LoginActivity.kt
@@ -1,13 +1,29 @@
 package com.example.messengerfirebase
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     override  fun  onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        login_button.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("Login","Attempt login with email/pw: $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+        }
+
+        back_to_registration_text_view.setOnClickListener{
+            finish()
+        }
 
 
     }
